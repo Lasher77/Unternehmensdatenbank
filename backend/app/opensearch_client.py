4bb997b4445eeefd1281b3a65aa88c3826e5b1aa
Lasher77 <sven.winkler@gmx.net>
2025-09-11 17:55:42 +0200

    Add OpenSearch auth and SSL settings

--- backend/app/opensearch_client.py
+++ backend/app/opensearch_client.py
@@ -10,4 +10,7 @@ def get_opensearch() -> OpenSearch:
     return OpenSearch(
         hosts=[{"host": settings.opensearch_host, "port": settings.opensearch_port}],
         http_compress=True,
+        use_ssl=settings.opensearch_use_ssl,
+        verify_certs=False,
+        basic_auth=(settings.opensearch_user, settings.opensearch_password),
     )
